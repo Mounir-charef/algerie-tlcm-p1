49f1a5092570fa41bd4047d997ca7ab242e5ec7f
Mounir <polomnr1@gmail.com>
2023-02-01 13:01:06 +0100

    created the user model start working on the login form

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User, Utilisateur, UtilisateurProfile
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Utilisateur)
+admin.site.register(UtilisateurProfile)
--- user/models.py
+++ user/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -22,8 +24,7 @@ class User(AbstractUser):
 
 class UtilisateurManager(BaseUserManager):
     def get_queryset(self, *args, **kwargs):
-        result = super().get_queryset(*args, **kwargs)
-        return result.filter(role=User.Role.UTILISATEUR)
+        return super().get_queryset(*args, **kwargs).filter(role=User.Role.UTILISATEUR)
 
 
 class Utilisateur(User):
@@ -34,5 +35,13 @@ class Utilisateur(User):
     class Meta:
         proxy = True
 
-    def welcome(self):
-        return "only for simple user"
+
+class UtilisateurProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    utilisateur_id = models.IntegerField(null=True, blank=True)
+
+
+@receiver(post_save, sender=Utilisateur)
+def create_user_profil(sender, instance, created, **kwargs):
+    if created and instance.role == "USER":
+        UtilisateurProfile.objects.create(user=instance)
