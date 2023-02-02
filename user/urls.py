08a723b0801877536378183e317db013babc483e
Mounir <polomnr1@gmail.com>
2023-02-02 15:06:58 +0100

    finishing with login and logout process next is to add information about the dot and display it

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     path('home/', views.homePage, name="home"),
     path('logout/', views.logoutUser, name="logout"),
     path('', views.loginPage, name="login"),
-    # path('projects/new/', views.createProject, name="projects"),
-    # path('project/<str:pk>/update/', views.updateProject, name="create-project"),5
-    # path('project/<str:pk>/delete/', views.deleteProject, name="delete-project"),
 ]
